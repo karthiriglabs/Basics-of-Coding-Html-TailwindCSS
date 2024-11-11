c9e3ad7a60aeb0e1bc56c7b87991db8a8b89ba57
Karthik Narayanan <162002738+karthiriglabs@users.noreply.github.com>
2024-11-11 18:55:52 +0530

    Update FeaturesSection.js

--- assets/js/sections/FeaturesSection.js
+++ assets/js/sections/FeaturesSection.js
@@ -26,26 +26,26 @@ class FeaturesSection extends HTMLElement {
             description="AI-powered applications can analyze vast amounts of visual data and provide designers with inspiration for their creative projects."
           ></accordion-element>
           <accordion-element
-            closedIcon="./assets/icons/draw_collage.svg"
-            openIcon="./assets/icons/draw_collage_light.svg"
+            closedIcon="./assets/icons/frame_inspect.svg"
+            openIcon="./assets/icons/frame_inspect_light.svg"
             label="Finding information"
             description="AI Ally acts as your digital detective, swiftly uncovering answers, facts, and insights from the vast web—making information hunting a breeze!"
           ></accordion-element>
           <accordion-element
-            closedIcon="./assets/icons/draw_collage.svg"
-            openIcon="./assets/icons/draw_collage_light.svg"
+            closedIcon="./assets/icons/broadcast_on_home.svg"
+            openIcon="./assets/icons/broadcast_on_home_light.svg"
             label="Home automation"
             description="AI Ally streamlines your home automation journey by providing personalized recommendations, troubleshooting tips, and step-by-step guides—all delivered with a touch of digital charm!"
           ></accordion-element>
           <accordion-element
-            closedIcon="./assets/icons/draw_collage.svg"
-            openIcon="./assets/icons/draw_collage_light.svg"
+            closedIcon="./assets/icons/animated_images.svg"
+            openIcon="./assets/icons/animated_images_light.svg"
             label="Entertainment"
             description="“AI Ally, your digital companion, curates movie recommendations, crafts personalized playlists, and even challenges you to brain-teasing puzzles—because life's better with a touch of whimsy!"
           ></accordion-element>
           <accordion-element
-            closedIcon="./assets/icons/draw_collage.svg"
-            openIcon="./assets/icons/draw_collage_light.svg"
+            closedIcon="./assets/icons/event_available.svg"
+            openIcon="./assets/icons/event_available_light.svg"
             label="Planning assistance"
             description="AI Ally, your trusty digital organizer, helps you map out schedules, set reminders, and even conjures up event ideas—because life's a grand adventure, and every good journey needs a roadmap!"
           ></accordion-element>
